915079219f306b83d90e7cfbc9b9819c378bdddb
Joshua Sanders <ninjosh2@gmail.com>
2020-01-05 06:17:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,8 +12,7 @@ end
 
 def words_five_letters_long(text)
   array = text.split(" ")
-  array.grep(/\b\w{5})
-
+  array.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
